3ef6de8b13500560be942b6df1c139afa28df04c
unknown <imagekunwar@gmail.com>
2022-03-03 22:49:36 +0545

    Socket programming accing Day and Time from server

--- src/np/edu/scst/socketprogramming/DayTimeClient.java
+++ src/np/edu/scst/socketprogramming/DayTimeClient.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package np.edu.scst.socketprogramming;
 
-/**
- *
- * @author Dell
- */
+package np.edu.scst.socketprogramming;
+import java.net.*;
+import java.io.*;
+import java.io.InputStreamReader;
+
 public class DayTimeClient {
-    
-}
+     public static void main(String[] args) {
+        String hostname = "time.nist.gov";
+        int port = 13;
+ 
+        try (Socket socket = new Socket(hostname, port)) {
+ 
+            InputStream input = socket.getInputStream();
+            InputStreamReader reader = new InputStreamReader(input);
+ 
+            int character;
+            StringBuilder data = new StringBuilder();
+ 
+            while ((character = reader.read()) != -1) {
+                data.append((char) character);
+            }
+ 
+            System.out.println(data);
+ 
+ 
+        } catch (UnknownHostException ex) {
+ 
+            System.out.println("Server not found: " + ex.getMessage());
+ 
+        } catch (IOException ex) {
+ 
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+    }
+    }
+   
