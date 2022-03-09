85d478b03ea51ed5b86341d20ae13c583c80ac3d
unknown <imagekunwar@gmail.com>
2022-03-09 10:15:07 +0545

    port scanner in network programming

---
+++ src/np/edu/scst/socketprogramming/LowPortScanner.java
@@ -0,0 +1,29 @@
+
+package np.edu.scst.socketprogramming;
+import java.net.*;
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class LowPortScanner {
+    public static void main(String[]args){
+        String host=args.length>0? args[0]:"localhost";
+        for(int i=1; i<1024; i++){
+            try{
+                Socket s=new Socket(host,i);
+                System.out.println("There is a server on port"+i+"of"+host);
+                s.close();
+            }
+            catch(UnknownHostException ex){
+                System.err.println(ex);
+                break;
+            }
+            catch(IOException ex){
+                System.out.println("Exception"+ex);
+                
+                
+            }
+        }
+    }
+}
+
