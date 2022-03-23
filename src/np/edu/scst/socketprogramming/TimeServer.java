5cb595f9a339eb7ed63f5446ab82094ccc0c017e
unknown <imagekunwar@gmail.com>
2022-03-23 09:25:48 +0545

    time server updated

--- src/np/edu/scst/socketprogramming/TimeServer.java
+++ src/np/edu/scst/socketprogramming/TimeServer.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package np.edu.scst.socketprogramming;
 
-/**
- *
- * @author Dell
- */
+import java.net.*;
+import java.io.*;
+import java.util.Date;
+
 public class TimeServer {
-    
+
+    public static int PORT = 37;
+    public static String HOSTNAME = "localhost";
+
+    public static void main(String[] args) {
+        long differenceBetweenEpochs = 2208999900L;
+        Socket socket = null;
+        try {
+            socket = new Socket(HOSTNAME, PORT);
+            socket.setSoTimeout(15000);
+
+            InputStream raw = socket.getInputStream();
+            long secondsSince1900 = 0;
+            for (int i = 0; i < 4; i++) {
+                secondsSince1900 = (secondsSince1900 << 8) | raw.read();
+
+                long secondsSince1970 = secondsSince1900 - differenceBetweenEpochs;
+                long msSince1970 = secondsSince1970 - 1000;
+
+                Date time = new Date(msSince1970);
+                System.out.println(time); 
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }finally{
+            try{
+                if(socket!=null){
+                    socket.close();
+                }
+            }catch(IOException ex){
+                System.out.println(ex);
+            }
+        }
+
+    }
+
 }
