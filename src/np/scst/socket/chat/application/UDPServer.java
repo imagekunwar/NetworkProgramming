d39d8de7d818309eedd6f1c5fda3e76ceb4a1f7a
unknown <imagekunwar@gmail.com>
2022-04-06 09:42:35 +0545

    bug fixed sendData

--- src/np/scst/socket/chat/application/UDPServer.java
+++ src/np/scst/socket/chat/application/UDPServer.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package np.scst.socket.chat.application;
 
-/**
- *
- * @author Dell
- */
+package np.scst.socket.chat.application;
+import java.io.*;
+import java.net.*;
+
 public class UDPServer {
+    public static void main (String[]args) throws Exception{
+        DatagramSocket serverSocket=new DatagramSocket(1234);
+        byte[]receiveData=new byte[1024];
+        byte[] sendData=new byte[1024];
+        
+        while(true){
+            DatagramPacket inPacket=new DatagramPacket( receiveData,receiveData.length);
+            serverSocket.receive(inPacket);
+            
+            String message= new String(inPacket.getData());
+            InetAddress clientAddress=inPacket.getAddress();
+            int clientPort=inPacket.getPort();
+            
+            DatagramPacket outPacket=new DatagramPacket(sendData,sendData.length,clientAddress,clientPort);
+            serverSocket.send(outPacket);
+            serverSocket.close();
+            
+            
+        }
+        
+    }
     
 }
