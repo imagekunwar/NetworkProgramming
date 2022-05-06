c839828a64de74fe7bf83ac922881157f9dda24a
unknown <imagekunwar@gmail.com>
2022-05-06 22:47:37 +0545

    netwrok

--- src/networkprogramming/UrlParsing.java
+++ src/networkprogramming/UrlParsing.java
@@ -6,13 +6,13 @@ public class UrlParsing {
         try{
         URL up =new URL("ftp://mp3:mp3@192.168.10.100:2100/%3a/");
         
-        System.out.println("schema::"+up.getProtocol());
-        System.out.println("schema::"+up.getUserInfo());
-        System.out.println("schema::"+up.getHost());
-        System.out.println("schema::"+up.getPort());
-        System.out.println("schema::"+up.getPath());
-        System.out.println("schema::"+up.getRef());
-        System.out.println("schema::"+up.getQuery());
+        System.out.println("protocol::"+up.getProtocol());
+        System.out.println("UserInfo::"+up.getUserInfo());
+        System.out.println("Host::"+up.getHost());
+        System.out.println("Port::"+up.getPort());
+        System.out.println("Path::"+up.getPath());
+        System.out.println("Refrence::"+up.getRef());
+        System.out.println("Query::"+up.getQuery());
         
         
     }
